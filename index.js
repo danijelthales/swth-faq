82d591de4a9d77200aa39e72624e1fe1d6e715bd
dgornjakovic <danijel.gornjakovic@prodyna.com>
2020-10-17 01:05:26 +0200

    fixes

--- index.js
+++ index.js
@@ -954,14 +954,14 @@ async function getStakingInfo() {
         /** @type {string[]} */
         var prices = await page.evaluate(() => {
 
-            var div = document.querySelectorAll('div.c513');
+            var div = document.querySelectorAll('p.jss173');
 
             var prices = []
             div.forEach(element => {
                 prices.push(element.textContent);
             });
 
-            div = document.querySelectorAll('div.c516');
+            div = document.querySelectorAll('div.jss179');
 
             div.forEach(element => {
                 prices.push(element.textContent);
@@ -973,9 +973,9 @@ async function getStakingInfo() {
         stakingAPR = prices[1];
         bondedSwth = prices[2];
         activeValidators = prices[3];
-        marketCap = prices[5];
-        totalSupply = prices[6];
-        tradeHubSupply = prices[7];
+        marketCap = prices[9];
+        totalSupply = prices[10];
+        tradeHubSupply = prices[11];
         totalSupply = replaceString(totalSupply, "SWTH", " SWTH");
         tradeHubSupply = replaceString(tradeHubSupply, "SWTH", " SWTH");
         browser.close()
